a4fc3f4328ebeb72645548ae28d8d0fafd0c2218
Aashish-fynd <ashishpal@gofynd.com>
2025-06-30 13:04:33 +0530

    docs: add openapi annotations and docs server

---
+++ server/src/docs/swagger.ts
@@ -0,0 +1,20 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const swaggerOptions = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bhandara API',
+      version: '1.0.0',
+    },
+    servers: [{ url: '/api' }],
+  },
+  apis: [path.join(__dirname, '../routes/**/*.ts')],
+};
+
+export const swaggerSpec = swaggerJsdoc(swaggerOptions);
